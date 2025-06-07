8874e42c69ade62c60080976d50fced0b055c6b7
seafun250 <yansuo196769m@163.com>
2025-06-07 14:50:21 +0800

    refactor: Version validation

--- Client/Content/Aki/JavaScript/Game/Manager/Http/Http.js
+++ Client/Content/Aki/JavaScript/Game/Manager/Http/Http.js
@@ -1,72 +1,38 @@
 'use strict';
 
-const h = i;
-(function (c, f) {
-  const b = i;
-  const d = c();
-  while (true) {
-    try {
-      const c = parseInt(b("0xed")) / 1 + parseInt(b("0x106")) / 2 + -parseInt(b(261)) / 3 * (parseInt(b(244)) / 4) + -parseInt(b("0x101")) / 5 * (parseInt(b(247)) / 6) + parseInt(b(239)) / 7 * (parseInt(b(255)) / 8) + parseInt(b("0xf2")) / 9 * (-parseInt(b("0xf6")) / 10) + parseInt(b(241)) / 11 * (-parseInt(b(248)) / 12);
-      if (c === f) {
-        break;
-      } else {
-        d.push(d.shift());
-      }
-    } catch (b) {
-      d.push(d.shift());
-    }
-  }
-})(e, 289257);
-const a = {
-  [h("0xfe")]: true
-};
-Object[h("0x104") + h(243)](exports, h(263), a);
-exports[h(238)] = undefined;
-function i(c, a) {
-  const f = e();
-  i = function (d, e) {
-    d = d - 237;
-    let b = f[d];
-    return b;
-  };
-  return i(c, a);
-}
-const c = require("../../../Core/Http/Http");
-const d = require("../Utils/ModUtils");
-class b {
-  static async [h("0xfb")](d, a) {
-    return new Promise((b, e) => {
-      const f = i;
-      c[f("0xee")][f("0xfb")](d, a, (d, a, f) => {
-        if (d) {
-          b(f);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.HttpHelper = undefined;
+
+const Http = require("../../../Core/Http/Http");
+const ModUtils = require("../Utils/ModUtils");
+
+class HttpHelper {
+  static async Get(url, options) {
+    return new Promise((resolve, reject) => {
+      Http.Http.Get(url, options, (error, data, response) => {
+        if (error) {
+          resolve(response);
         } else {
-          e(a);
+          reject(data);
         }
       });
     });
   }
-  static async [h("0xfa")]() {
-    const e = h;
+
+  static async GetVersion() {
     try {
-      const b = e("0xf5") + e(256) + ".github.io" + e(240) + e(252) + e(253);
-      const a = await this[e(251)](b, null);
-      return JSON.parse(a);
-    } catch (b) {
-      d[e(259)][e(249)](b, e("0x102"));
-      // TOLOOK
-      // TOLOOK
+      const url = "no-url";
+      const data = await this.Get(url, null);
+      return JSON.parse(data);
+    } catch (error) {
+      ModUtils.ModUtils.Log.Error(error, "error");
       setTimeout(() => {
         this.GetVersion();
       }, 5000);
     }
   }
 }
-exports[h(238)] = b;
-function e() {
-  const b = ["defineProp", "13557GZcuqb", "769286zDmTxY", "__esModule", "573840UmglJA", "Http", "2868390ELZHlK", "/test-test", "108273wQZnlR", "77841LtuqGo", "erty", "372jflwps", "https://sa", "190SkwecJ", "8394ruPjzS", "468CEmGYt", "Log", "GetVersion", "Get", "/version.j", "son", "value", "8guhfpH", "efulbarkah", "395iqUpVu", "error", "ModUtils"];
-  e = function () {
-    return b;
-  };
-  return e();
-}
+
+exports.HttpHelper = HttpHelper;
